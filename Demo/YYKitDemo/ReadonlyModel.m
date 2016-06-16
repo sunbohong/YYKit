a9ce6072bf2c4de0ab2a5be932c5af336e6c48d5
sunbohong <sunbohong@gmail.com>
2016-06-16 09:46:27 +0800

    修改测试方案

--- Demo/YYKitDemo/ReadonlyModel.m
+++ Demo/YYKitDemo/ReadonlyModel.m
@@ -11,19 +11,15 @@
 
 @interface ReadonlyModel ()<YYModel>
 
-@property (nonatomic, copy) NSString *privatepro0;
-@property (nonatomic, copy) NSString *privatepro1;
+@property (nonatomic, copy, readwrite) NSString *pro1;
 
 @end
 
 @implementation ReadonlyModel
 
-+ (NSDictionary<NSString *, id> *)modelCustomPropertyMapper {
-    return @{@"privatepro0":@"pro0", @"pro1":@"pro1"};
-}
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"pro0==%@,pro1==%@,pro2==%@", self.privatepro0, self.privatepro1,pro2];
+    return [NSString stringWithFormat:@"pro0==%@,pro1==%@,pro2==%@", self.pro0, self.pro1, pro2];
 }
 
 @end
